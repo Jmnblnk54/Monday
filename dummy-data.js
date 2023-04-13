55dc49f2fda673e58be68bfaf68a1196db6c6807
Joshua Blank <jmnblnk@yahoo.com>
2023-04-13 11:53:07 -0400

    Near completed project with bugs to work out

---
+++ dummy-data.js
@@ -0,0 +1,54 @@
+const tickets = [
+    {
+      category: 'Q1 2022',
+      color: 'red',
+      title: 'NFT Safety 101 Video',
+      owner: 'That Guy',
+      avatar: 'https://stock.adobe.com/images/closeup-portrait-of-funny-ginger-cat-wearing-sunglasses-isolated-on-light-cyan-copyspace/303624505',
+      status: 'done',
+      priority: 5,
+      progress: 40,
+      description:
+        'Make a video showcasing how to work with NFTs safely, including how to know if one is not genuine.',
+      timestamp: '2022-02-11T07:36:17+0000',
+    },
+    {
+      category: 'Q1 2022',
+      color: 'red',
+      title: 'Build and Sell an AI Model',
+      owner: 'Jmn Blnk',
+      avatar: 'https://stock.adobe.com/images/closeup-portrait-of-funny-ginger-cat-wearing-sunglasses-isolated-on-light-cyan-copyspace/303624505',
+      status: 'working on it',
+      priority: 4,
+      progress: 2,
+      description:
+        'Show how easy it is to work with machine Learning and show people how to make money by building and selling an AI API',
+      timestamp: '2022-02-13T07:36:17+0000',
+    },
+    {
+      category: 'Q1 2022',
+      color: 'red',
+      title: 'My top tools for 2022',
+      owner: 'Josh Blank',
+      avatar: 'https://stock.adobe.com/images/closeup-portrait-of-funny-ginger-cat-wearing-sunglasses-isolated-on-light-cyan-copyspace/303624505',
+      status: 'stuck',
+      priority: 10,
+      progress: 6,
+      description:
+        'Share my top tools for 2022 with everyone in a 15 minute video.',
+      timestamp: '2022-02-16T07:36:17+0000',
+    },
+    {
+      category: 'Q2 2022',
+      color: 'blue',
+      title: 'Frogger in React',
+      owner: 'Joshua Blank',
+      avatar: null,
+      status: '',
+      priority: 2,
+      progress: 0,
+      description:
+        'Make a video showing how to build the popular retro game Frogger in React.',
+      timestamp: '2022-02-21T07:36:17+0000',
+    },
+  ]
